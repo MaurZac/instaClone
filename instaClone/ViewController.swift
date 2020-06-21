ddf2fcb508ecfa60b0cf790070dd299a6d6a44c7
Mau <maur.mx@gmail.com>
2020-06-21 16:59:05 -0500

    Initial Commit

---
+++ instaClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  instaClone
+//
+//  Created by MaurZac on 21/06/20.
+//  Copyright © 2020 MaurZac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
