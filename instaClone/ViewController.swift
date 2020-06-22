35d39462b03f16b7faaad7d969444ebcacbcbd2b
Mau <maur.mx@gmail.com>
2020-06-21 19:24:46 -0500

    proyecto limpio y estable con pod file y firebase

--- instaClone/ViewController.swift
+++ instaClone/ViewController.swift
@@ -15,9 +15,6 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-    //comentarios aqui
-    func muta() {
-        print("lol")
-    }
+  
 }
 
