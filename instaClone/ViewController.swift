814b6566593edaf83d3c38abb24beb90eb21d78d
Mau <maur.mx@gmail.com>
2020-06-25 11:18:55 -0500

    Stable register button y firebase connected

--- instaClone/ViewController.swift
+++ instaClone/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
     @IBOutlet weak var emailText: UITextField!
@@ -21,6 +22,29 @@ class ViewController: UIViewController {
         performSegue(withIdentifier: "toFeedVC", sender: nil)
     }
     @IBAction func register(_ sender: UIButton) {
+        
+        if emailText.text != "" && passwordText.text != "" {
+            Auth.auth().createUser(withEmail: emailText.text!, password: passwordText.text!) { (authdata, error) in
+                if error != nil{
+                    self.errorAlert(errorInput: "Error!", messageInput: error?.localizedDescription ?? "Error at firebase register")
+                }else{
+                    self.performSegue(withIdentifier: "toFeedVC", sender: nil)
+                }
+            }
+            
+            
+        }else{
+           errorAlert(errorInput: "Error!", messageInput: "username/password")
+        }
+        
+    }
+    
+    
+    func errorAlert(errorInput: String, messageInput: String){
+        let alert = UIAlertController(title: errorInput , message: messageInput, preferredStyle: UIAlertController.Style.alert)
+                   let okBtn = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
+                   alert.addAction(okBtn)
+                   self.present(alert, animated: true, completion: nil)
     }
     
 }
