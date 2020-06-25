7a3b3b60f1c3802ad4a7529f93d69ad1f5c5c59a
Mau <maur.mx@gmail.com>
2020-06-25 09:25:21 -0500

    3 pantallas estables para el clone

--- instaClone/ViewController.swift
+++ instaClone/ViewController.swift
@@ -9,12 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var emailText: UITextField!
+    @IBOutlet weak var passwordText: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     
-  
+    @IBAction func login(_ sender: UIButton) {
+        performSegue(withIdentifier: "toFeedVC", sender: nil)
+    }
+    @IBAction func register(_ sender: UIButton) {
+    }
+    
 }
 
